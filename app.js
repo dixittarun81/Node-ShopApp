3993602d955939c2900ee954bd67d2d2cde41813
tarun dixit <dixittarun81@gmail.com>
2020-06-20 10:55:16 +0530

    setting mongodb crud

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');//for parsing chunk of streams
 const app = express();
 
+const mongoose = require('mongoose');
+
 const mongoConnect = require('./util/database').mongoConnect;
 
 
@@ -15,8 +17,9 @@ app.set('views', 'views');
 
 
 const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,13 +28,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req,res,next) => {
-  next();
+  User.findById('5eeadc139313c1d6f12c66ee')
+    .then(user => {
+      req.user = new User(user.name,user.email,user.cart,user._id);
+      next();
+    })
+    .catch(err => console.log(err));
+ 
 });
 
 //Applying middleware for route
 //For admin all the subroutes will begin after /admin
 app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
+app.use(shopRoutes);
 
 
 
